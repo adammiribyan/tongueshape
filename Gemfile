2f45ac4e3b99cc7274c293bf34177779e2b117c1
Adam <adam.miribyan@gmail.com>
2011-03-21 01:55:26 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+gem 'mysql2'
+gem 'bcrypt-ruby', :require => 'bcrypt'
+
+gem 'haml'
+gem 'haml-rails'
+gem 'less'
+gem 'simple_form'
+gem 'russian'
+
+gem 'jquery-rails', '~>0.2'
+
+gem 'capistrano'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tongueshape::Application.config.secret_token = '622745ed6fd570af7bf50240e59ef0c8a5691e5e0ea934ba01818c4c5537bab1c79104aa1de8f87e4214eb39b68224e17bc468a14ed334f90c66061b11ecc810'
